4f0f5603f5c35ed29f53f911c35139cc2529f420
ConsoleArgonaut <vanderheide.michael@gmail.com>
2016-10-28 15:18:03 +0200

    Defined Enemy, Added Enemytype

--- TTOD/src/com/company/Enemy.java
+++ TTOD/src/com/company/Enemy.java
@@ -4,4 +4,11 @@ package com.company;
  * Created by Kaiden on 28.10.2016.
  */
 public class Enemy {
+    public Types.Enemy Type;
+    public Enemy EvolvesTo;
+
+    public Enemy() {
+        Type = Types.Enemy.Creature;
+        EvolvesTo = null;
+    }
 }
