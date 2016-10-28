d54e2eab73d898eb8e116bb78c98c16d3eec4a1e
ConsoleArgonaut <vanderheide.michael@gmail.com>
2016-10-28 15:01:35 +0200

    Added Character Definition

--- TTOD/src/com/company/Character.java
+++ TTOD/src/com/company/Character.java
@@ -4,4 +4,58 @@ package com.company;
  * Created by Kaiden on 28.10.2016.
  */
 public class Character {
+
+    public String Name;
+
+    public float Life;
+
+    public float BaseDefense;
+
+    public float BaseAttack;
+
+    public float Inteligence;
+
+    public float CritChance;
+
+    public float BlockChance;
+
+    public float Luck;
+
+    public int Initiative;
+
+    public int MaxItems;
+
+    public float MaxDefense;
+
+    public float MaxLife;
+
+    public float MaxAttack;
+
+    public float GEIL;
+
+    public Item Armor;
+
+    public Item Weapon;
+
+    public Item[] Items;
+
+    public Character() {
+        Name = "";
+        Life = 0;
+        BaseDefense = 0;
+        BaseAttack = 0;
+        Inteligence = 0;
+        CritChance = 0;
+        BlockChance = 0;
+        Luck = 0;
+        Initiative = 0;
+        MaxItems = 0;
+        MaxDefense = 0;
+        MaxLife = 0;
+        MaxAttack = 0;
+        GEIL = 0;
+        Armor = new Item();
+        Weapon = new Item();
+        Items = new Item[0];
+    }
 }
