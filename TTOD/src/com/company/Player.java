f7ec32d0254b0e4035c0fdc5edd631fc0f4402b5
Kaiden-PC\Kaiden <kaiden3991@gmail.com>
2016-10-28 15:16:40 +0200

    Defined Player.java

--- TTOD/src/com/company/Player.java
+++ TTOD/src/com/company/Player.java
@@ -4,4 +4,27 @@ package com.company;
  * Created by Kaiden on 28.10.2016.
  */
 public class Player {
+  public static Player getOurInstance() {
+    return ourInstance;
+  }
+
+  public static void setOurInstance(Player ourInstance) {
+    Player.ourInstance = ourInstance;
+  }
+
+  private static Player ourInstance = new Player();
+
+  public static Player getInstance() {
+    return ourInstance;
+  }
+
+  private Player() {
+  }
+
+  public int TimesCleared;
+
+  public int Charisma;
+
+  public int FloorCount;
+
 }
