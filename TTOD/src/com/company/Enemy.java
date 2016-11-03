1e3dd1060f1662fc7207c69969c35fed2039c1d1
Kaiden-PC\Drake <kaiden3991@gmail.com>
2016-11-03 18:09:06 +0100

    Added Class EnemyController.java Edited Class Enemy.java added getter and setter for Type

--- TTOD/src/com/company/Enemy.java
+++ TTOD/src/com/company/Enemy.java
@@ -4,6 +4,7 @@ package com.company;
  * Created by Kaiden on 28.10.2016.
  */
 public class Enemy extends Character{
+
     public Types.Enemy Type;
     public Enemy EvolvesTo;
 
@@ -11,4 +12,25 @@ public class Enemy extends Character{
         Type = Types.Enemy.Creature;
         EvolvesTo = null;
     }
+
+    public Types.Enemy getType() {
+        return Type;
+    }
+
+    public void setType(Types.Enemy type) {
+        Type = type;
+    }
 }
+
+/**
+legendary dragon
+goblin
+ork
+kobold
+dragon
+skeleton
+treant
+spiders
+shepard
+bandit
+*/
---
+++ TTOD/src/com/company/EnemyController.java
@@ -0,0 +1,24 @@
+package com.company;
+
+/**
+ * Created by Kaiden on 28.10.2016.
+ */
+public class EnemyController extends Character && Enemy{
+
+    public EnemyController() {
+
+    }
+}
+
+/**
+ legendary dragon
+ goblin
+ ork
+ kobold
+ dragon
+ skeleton
+ treant
+ spiders
+ shepard
+ bandit
+ */
