3eef560c22c2ab55336b4fa040294a2b65a8d5d3
ConsoleArgonaut <vanderheide.michael@gmail.com>
2016-11-04 14:36:48 +0100

    Added empty Test to Trader

---
+++ TTOD/JUnitTests/TraderTest.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Michael on 04.11.2016.
+ */
+public class TraderTest {
+    @Test
+    public void buyItem() throws Exception {
+
+    }
+
+    @Test
+    public void getItemSellPrice() throws Exception {
+
+    }
+
+    @Test
+    public void getItemBuyPrice() throws Exception {
+
+    }
+
+    @Test
+    public void sellItem() throws Exception {
+
+    }
+
+}
