7756a80127f6f8d893ff98917eb09dadff0ba68e
ConsoleArgonaut <vanderheide.michael@gmail.com>
2016-11-05 19:32:15 +0100

    Defined Game, and Town

--- TTOD/src/com/company/Game.java
+++ TTOD/src/com/company/Game.java
@@ -1,16 +1,58 @@
 package com.company;
 
+import java.util.Random;
+
 /**
  * Created by pstieger on 28.10.2016.
  */
 public class Game {
+    /** The Town */
+    public Town Town;
+
+    /** The Dungeon */
     public Dungeon Dungeon;
 
-    public void startNewGame(){
-
+    public Game(){
+        Town = new Town();
+        Dungeon = new Dungeon();
     }
 
-    public void initializePlayer(){
+    /** Initializes everything needed to start a new game */
+    public void startNewDefaultGame(){
+        initializeDefaultPlayer();
+        Dungeon.generateDefaultFloors();
+        Town.generateDefaultTraders();
+    }
 
+    /** Initializes the Player */
+    public void initializeDefaultPlayer(){
+        Player.getOurInstance().FloorCount = 0;
+        Player.getOurInstance().setMaxLife(1500);
+        Player.getOurInstance().Attack = 60;
+        Player.getOurInstance().Luck = new Random().nextInt(-100);
+        Player.getOurInstance().Charisma = new Random().nextInt(-100);
+        Player.getOurInstance().TimesCleared = 0;
+        Player.getOurInstance().BlockChance = 60;
+        Player.getOurInstance().CritChance = 60;
+        Player.getOurInstance().Defense = 60;
+        Player.getOurInstance().Initiative = new Random().nextInt(-100);
+        Player.getOurInstance().MaxItems = 20;
+        Player.getOurInstance().GEIL = 0;
+        Item cape = new Item();
+        cape.Type = Types.ItemType.Armor;
+        cape.Name = "Hero cape";
+        cape.Value = 300;
+        cape.Defense = 30;
+        cape.MaxCount = 1;
+        Player.getOurInstance().addItem(cape);
+        Player.getOurInstance().setArmor(cape);
+        Item stick = new Item();
+        stick.Name = "Fancy stick of fancyness";
+        stick.Type = Types.ItemType.Sword;
+        stick.Attack = 20;
+        stick.Value = 50;
+        stick.MaxCount = 1;
+        Player.getOurInstance().addItem(stick);
+        Player.getOurInstance().setWeapon(stick);
     }
 }
--- TTOD/src/com/company/Town.java
+++ TTOD/src/com/company/Town.java
@@ -6,19 +6,23 @@ import java.util.ArrayList;
  * Created by pstieger on 28.10.2016.
  */
 public class Town {
-    public ArrayList<Trader> Traders = new ArrayList<Trader>();
+    public ArrayList<Trader> Traders;
 
-    public void goSleeping(){
-
+    public Town(){
+        Traders = new ArrayList<Trader>();
     }
 
-    public Trader getTrader(Types.Trader traderType){
-        for(Trader t : Traders){
-            if(t.Type == traderType){
-                return t;
-            }
-        }
+    public void generateDefaultTraders(){
+        Trader blacksmith = new Trader();
+        blacksmith.Name = "Wilfried the blacksmith";
+        blacksmith.Charisma = 30;
+        blacksmith.GEIL = 400;
+        //Add all Items to the Trader
+        Traders.add(blacksmith);
+    }
 
-        return null;
+    /** Rest, and fill up players life */
+    public void goSleeping(){
+        Player.getOurInstance().Life = Player.getOurInstance().MaxLife;
     }
 }
