6db8854205550a53789b00e5a2b4005d902a8284
ConsoleArgonaut <vanderheide.michael@gmail.com>
2016-11-08 19:03:20 +0100

    Added Game Tests

--- TTOD/JUnitTests/GameTest.java
+++ TTOD/JUnitTests/GameTest.java
@@ -1,3 +1,6 @@
+import com.company.Game;
+import com.company.Player;
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,18 +10,36 @@ import static org.junit.Assert.*;
  */
 public class GameTest {
     @Test
-    public void startNewDefaultGame() throws Exception {
-
+    public void startNewDefaultGame() {
+        Game game = new Game();
+        game.startNewDefaultGame();
+        Assert.assertTrue(game.Dungeon.Floors.size() == 8);
+        Assert.assertTrue(game.Town.Traders.size() > 0);
+        Assert.assertTrue(Player.getOurInstance().Life > 0);
     }
 
     @Test
-    public void initializeDefaultPlayer() throws Exception {
-
+    public void initializeDefaultPlayer() {
+        Game game = new Game();
+        Player.getOurInstance().Name = "Test Player";
+        game.initializeDefaultPlayer();
+        Assert.assertTrue(Player.getOurInstance().MaxLife == Player.getOurInstance().Life);
+        Assert.assertTrue(Player.getOurInstance().Life > 0);
+        Assert.assertTrue(Player.getOurInstance().Defense < 100 && Player.getOurInstance().Defense > 0);
+        Assert.assertTrue(Player.getOurInstance().Attack < 100 && Player.getOurInstance().Attack > 0);
+        Assert.assertEquals(Player.getOurInstance().Name, "Test Player");
     }
 
     @Test
-    public void initializeGodModePlayer() throws Exception {
-
+    public void initializeGodModePlayer() {
+        Game game = new Game();
+        Player.getOurInstance().Name = "Test Player";
+        game.initializeGodModePlayer();
+        Assert.assertTrue(Player.getOurInstance().MaxLife == Player.getOurInstance().Life);
+        Assert.assertTrue(Player.getOurInstance().Life > 100);
+        Assert.assertTrue(Player.getOurInstance().Defense > 100);
+        Assert.assertTrue(Player.getOurInstance().Attack > 100);
+        Assert.assertEquals(Player.getOurInstance().Name, "Test Player");
     }
 
 }
