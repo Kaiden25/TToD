d8a36a3acf8e994fdfe8a46d8b4507637f5e97e9
ConsoleArgonaut <vanderheide.michael@gmail.com>
2016-11-08 20:09:13 +0100

    Added Floor Test

--- TTOD/JUnitTests/FloorTest.java
+++ TTOD/JUnitTests/FloorTest.java
@@ -1,3 +1,6 @@
+import com.company.Enemy;
+import com.company.Floor;
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,13 +10,22 @@ import static org.junit.Assert.*;
  */
 public class FloorTest {
     @Test
-    public void nextCombat() throws Exception {
-
+    public void nextCombat() {
+        Floor floor = new Floor();
+        Enemy e = new Enemy();
+        floor.PossibleEnemies.add(e);
+        Assert.assertTrue(floor.nextCombat().CurrentEnemy == e);
     }
 
     @Test
-    public void canLeave() throws Exception {
-
+    public void canLeave() {
+        Floor floor = new Floor();
+        floor.PossibleEnemies.add(new Enemy());
+        boolean leftOnce = false;
+        for (int i = 0; i < 900; i++) {
+            if(floor.canLeave())
+                leftOnce = true;
+        }
     }
 
 }
