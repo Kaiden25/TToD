96e6ef96966be636ea4a4d51b7a325af16826f37
ConsoleArgonaut <vanderheide.michael@gmail.com>
2016-11-08 18:21:21 +0100

    Added Town Tests

--- TTOD/JUnitTests/TownTest.java
+++ TTOD/JUnitTests/TownTest.java
@@ -1,3 +1,9 @@
+import com.company.Player;
+import com.company.Town;
+import com.company.Trader;
+import com.company.Types;
+import com.sun.org.apache.xpath.internal.operations.Equals;
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,13 +13,23 @@ import static org.junit.Assert.*;
  */
 public class TownTest {
     @Test
-    public void generateDefaultTraders() throws Exception {
-
+    public void generateDefaultTraders() {
+        Town t = new Town();
+        for (Trader trader: t.Traders) {
+            Assert.assertTrue(trader.GEIL > 0);
+            Assert.assertTrue(trader.Charisma > 0);
+            Assert.assertTrue(trader.Items.size() > 0);
+            Assert.assertTrue(trader.Name != new Trader().Name);
+        }
     }
 
     @Test
-    public void goSleeping() throws Exception {
-
+    public void goSleeping() {
+        Player.getOurInstance().setMaxLife(100);
+        Player.getOurInstance().Life = 50;
+        Town t = new Town();
+        t.goSleeping();
+        Assert.assertTrue(Player.getOurInstance().Life == Player.getOurInstance().MaxLife);
     }
 
 }
