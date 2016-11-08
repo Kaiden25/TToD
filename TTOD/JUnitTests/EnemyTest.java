51a145c0aebdb952562b5034aff36230864c5cb8
ConsoleArgonaut <vanderheide.michael@gmail.com>
2016-11-08 20:16:52 +0100

    Added Enemy Test

--- TTOD/JUnitTests/EnemyTest.java
+++ TTOD/JUnitTests/EnemyTest.java
@@ -1,3 +1,6 @@
+import com.company.Enemy;
+import com.company.Types;
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,18 +10,35 @@ import static org.junit.Assert.*;
  */
 public class EnemyTest {
     @Test
-    public void getType() throws Exception {
-
+    public void getType() {
+        Enemy e = new Enemy();
+        e.Type = Types.Enemy.Dragon;
+        Assert.assertTrue(e.getType() == Types.Enemy.Dragon);
     }
 
     @Test
-    public void setType() throws Exception {
-
+    public void setType() {
+        Enemy e = new Enemy();
+        e.setType(Types.Enemy.Dragon);
+        Assert.assertTrue(e.Type == Types.Enemy.Dragon);
     }
 
     @Test
-    public void cloneEnemy() throws Exception {
-
+    public void cloneEnemy() {
+        Enemy e = new Enemy();
+        e.Type = Types.Enemy.Dragon;
+        e.Name = "Lucifer";
+        e.Attack = 100;
+        e.Defense = 100;
+        e.Luck = 100;
+        e.MaxLife = 2000;
+        Enemy e2 = e.cloneEnemy();
+        Assert.assertTrue(e2.Name == e.Name);
+        Assert.assertTrue(e2.Attack == e.Attack);
+        Assert.assertTrue(e2.Type == e.Type);
+        Assert.assertTrue(e2.Defense == e.Defense);
+        Assert.assertTrue(e2.Luck == e.Luck);
+        Assert.assertTrue(e2.MaxLife == e.MaxLife);
     }
 
 }
